b7830e27069b0c33c8f8423545706cba0d436e40
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2018-05-14 21:21:34 +0300

    0.25

--- src/BsMultiSelect.es8.js
+++ src/BsMultiSelect.es8.js
@@ -16,8 +16,8 @@ const BsMultiSelect = ((window, $, Popper) => {
         //usePopper: true,
         selectedPanelMinHeight: "calc(2.25rem + 2px)",
         selectedPanelReadonlyBackgroundColor: "#e9ecef",
-        selectedPanelValidBoxShadow: " 0 0 0 0.2rem rgba(40, 167, 69, 0.5)",
-        selectedPanelInvalidBoxShadow: "0 0 0 0.2rem rgba(220, 53, 69, 0.5)",
+        selectedPanelValidBoxShadow: " 0 0 0 0.2rem rgba(40, 167, 69, 0.25)",
+        selectedPanelInvalidBoxShadow: "0 0 0 0.2rem rgba(220, 53, 69, 0.25)",
         filterInputColor: "#495057",
 
 
