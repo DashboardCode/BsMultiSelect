6a2e30f37b8ad59e06319259cebbca7537853c00
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2019-02-25 18:38:06 +0200

    ver change

--- dist/js/BsMultiSelect.js
+++ dist/js/BsMultiSelect.js
@@ -1,5 +1,5 @@
 /*!
-  * DashboardCode BsMultiSelect v0.2.19 (https://dashboardcode.github.io/BsMultiSelect/)
+  * DashboardCode BsMultiSelect v0.2.20 (https://dashboardcode.github.io/BsMultiSelect/)
   * Copyright 2017-2019 Roman Pokrovskij (github user rpokrovskij)
   * Licensed under APACHE 2 (https://github.com/DashboardCode/BsMultiSelect/blob/master/LICENSE)
   */
