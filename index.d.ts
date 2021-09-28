36a6450956726d25a19e25847f17f16043a5bf31
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2021-09-28 23:59:49 +0300

    beta 16

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-export * from './src';
+declare module '@dashboardcode/bsmultiselect'{
+    // example
+    export function ModuleFactory(environment:any): any
+    //export declare type ModuleFactory = (environment: any) => any;
+}
+
+
+//export * from './src';
--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@dashboardcode/bsmultiselect'{
-    // example
-    export function ModuleFactory(environment:any): any
-    //export declare type ModuleFactory = (environment: any) => any;
-}
-
-// declare module '*';
-
